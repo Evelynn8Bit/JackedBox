79a0878c167cb0dd0c8ab58bd2b7f05fee0f31ea
Ev Hudson <evenlynn.h22@gmail.com>
2025-02-10 13:01:09 -0800

    Add classNames to collapse menus

--- src/UI.jsx
+++ src/UI.jsx
@@ -3,15 +3,15 @@ export const UI = () => {
   return (
     <main id="UI">
       <MenuPannel id={"other"} className="half collapse-x">
-        <h2>Other</h2>
-        <div className="menu__item-content">Content</div>
+        <h2 class="menu__item--title">Other</h2>
+        <div className="menu__item--content">Content</div>
       </MenuPannel>
       <MenuPannel id={"user"} className="full collapse-y">
         <div className="profile">
           <div className="pfp"></div>
-          <h2>Hi</h2>
+          <h2 className="menu__item--title">Hi</h2>
         </div>
-        <div className="menu__item-content">Content</div>
+        <div className="menu__item--content">Content</div>
       </MenuPannel>
     </main>
   );
